748b7c1d6a2481ce30f2efa46bd0f2748ee10cf2
Subrahmanyam <subrahmanyam.g@evergent.com>
2026-02-19 07:48:05 +0530

    fix api routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://tradesk-production.up.railway.app",
-        changeOrigin: true,
-      },
-    },
-  },
+  build: {
+    outDir: "dist"
+  }
 });
